932a1a2cd377566576b48539aa06c10421a9abd4
erichon <erichon18@gmail.com>
2023-02-08 15:59:21 +0800

    20230208 full working version with drawer control program

--- main.ts
+++ main.ts
@@ -1,16 +1,31 @@
 input.onButtonPressed(Button.A, function () {
-    radio.sendValue("forward", speed)
-    basic.pause(500)
-    radio.sendValue("stop", speed)
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        # . # . #
+        . . # . .
+        . . # . .
+        `)
+    radio.sendString("forward")
 })
 input.onButtonPressed(Button.AB, function () {
-    radio.sendValue("stop", 0)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    radio.sendString("stop")
 })
 input.onButtonPressed(Button.B, function () {
-    radio.sendValue("backward", speed)
-    basic.pause(2000)
-    radio.sendValue("stop", speed)
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        # . # . #
+        . # # # .
+        . . # . .
+        `)
+    radio.sendString("backward")
 })
-let speed = 0
 radio.setGroup(1)
-speed = 100
